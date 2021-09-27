a4920a95b580c14dd4ec9c097ee470c6e19f016f
Ruud Jungbacker <rju@woodwing.com>
2021-09-27 20:19:34 +0200

    Fix call to syntaxHighlight.

--- plugin.js
+++ plugin.js
@@ -83,7 +83,7 @@ class AjaxCallFromExternalSource {
                     'jsonrpc': '2.0'
                 })
             }).then((result) => {
-                this.$('#inbox-result').text(syntaxHighlight(JSON.stringify(result)));
+                this.$('#inbox-result').text(this.syntaxHighlight(JSON.stringify(result)));
             }).catch((error) => {
                 this.$('#inbox-result').text('Error: ' + error.status + ' - ' + error.statusText);
                 console.error('Error occurred: ', error);
